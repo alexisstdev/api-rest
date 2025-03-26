9ba4fad161c8fefe59ec1595b2dbd2e4b2e3056a
alexisstdev <alexissanmiguel03@gmail.com>
2025-03-26 09:35:52 -0500

    Refactor HTTPS server setup to improve readability and streamline certificate loading

--- parcial-2/https/src/main.ts
+++ parcial-2/https/src/main.ts
@@ -1,22 +1,20 @@
-import { createApp } from './app';
-import { ENV } from './config/env';
-import * as https from 'node:https';
-import * as fs from 'node:fs';
-import * as path from 'node:path';
+import { createApp } from "./app";
+import { ENV } from "./config/env";
+import * as https from "node:https";
+import * as fs from "node:fs";
+import * as path from "node:path";
 
 const app = createApp();
 
-const options = {
-	key: fs.readFileSync(path.join(__dirname, '../certs/key.pem')),
-	cert: fs.readFileSync(path.join(__dirname, '../certs/cert.pem')),
-};
+const key = fs.readFileSync(path.join(__dirname, "../certs/key.pem"), "utf8");
+const cert = fs.readFileSync(path.join(__dirname, "../certs/cert.pem"), "utf8");
 
-const server = https.createServer(options, app);
+const server = https.createServer({ key, cert }, app);
 
-server.listen(ENV.PORT, '::', async () => {
+server.listen(ENV.PORT, "::", async () => {
 	console.log(
-		`HTTPS Server is running on port https://localhost:${ENV.PORT} at ${new Date().toLocaleDateString()}`,
+		`Server is running on port https://localhost:${ENV.PORT} at ${new Date().toLocaleDateString()}`,
 	);
 
-	app.emit('listening');
+	app.emit("listening");
 });
